7470de2640472f2c6c45a3a6b6293f793efbf145
Neil Bowers <neil@bowers.com>
2014-05-05 23:34:28 +0100

    Path in regex needed to be wrapped with \Q .. \E for Windows

--- lib/BackPAN/Index/Create.pm
+++ lib/BackPAN/Index/Create.pm
@@ -57,7 +57,7 @@ sub create_backpan_index
 
     foreach my $path ($rule->all($author_dir)) {
         my $tail = $path;
-           $tail =~ s!^${stem}[^A-Za-z0-9]+!!;
+           $tail =~ s!^\Q${stem}\E[^A-Za-z0-9]+!!;
            $tail =~ s!\\!/!g if $^O eq 'MSWin32';
         my @stat = stat($path);
         my $time = $stat[9];
